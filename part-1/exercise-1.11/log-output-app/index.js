20e04050e20ec8087e8f6231b7194317e1831517
Kimmo Löfman <ext.kimmo.lofman@varma.fi>
2024-10-08 19:22:11 +0300

    Added exercise-1.11

---
+++ part-1/exercise-1.11/log-output-app/index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+const directory = path.join('/', 'usr', 'src', 'app', 'log');
+const filePath = path.join(directory, 'counter.txt');
+let counter = 0;
+
+const stringGenerator = (length)=> {
+    const chars = 'abcdefghijklmnopqrstuvwxyz';
+    let result = '';
+    const charLength = chars.length;
+
+    for (let i = 0; i < length; i++) {
+        result += chars.charAt(Math.floor(Math.random() * charLength));
+    }
+
+    return result;
+}
+const randomString = stringGenerator(20);
+
+function getCounter(callback) {
+    console.log(filePath);
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                callback('File not found.');
+            } else {
+                callback('Error while reading');
+            }
+            return;
+        }
+
+        const lines = data.trim().split('\n');
+        const counter = lines[lines.length - 1]; 
+
+        callback(null, counter);
+    });
+}
+
+
+app.get('/', (req, res) => {
+    getCounter((err, counter) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            const timestamp = new Date().toISOString(); 
+            res.send(`${timestamp}: ${randomString}\n Ping / Pongs: ${counter}`);
+        }
+    });
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Server started in port ${port}`);
+  });
---
+++ part-1/exercise-1.11/pingpong-app/app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+const directory = path.join('/', 'usr', 'src', 'app', 'log');
+const filePath = path.join(directory, 'counter.txt');
+
+if (!fs.existsSync(directory)) {
+    fs.mkdirSync(directory, { recursive: true });
+}
+
+let counter = 0;
+
+app.use((req, res, next)=>{
+    
+    if (req.originalUrl && req.originalUrl.split("/").pop() === 'favicon.ico') {
+        return res.sendStatus(204);
+    }
+    
+    counter++;
+    fs.appendFile(filePath, `${counter}\n`, (err) => {
+        if (err) {
+            console.error('Error while writing to the file:', err);
+        }
+    });
+    next();
+});
+
+app.get('/pingpong',(req, res, next) =>{
+    res.write('<html>');
+    res.write('<head><title>Ping Pong</title></head>');
+    res.write('<body><h1>Ping pong page</h1>');
+    res.write(`<p>Counter: ${counter}`);
+    res.write('</body>');
+    res.write('</html');
+    return res.end();
+});
+
+
+app.listen(port, () => {
+    console.log(`Server started in port ${port}`);
+  });
