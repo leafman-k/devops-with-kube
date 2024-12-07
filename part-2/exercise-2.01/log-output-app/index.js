eb4cb6094cb238d9d3809c2239b6a0b3f77f098d
Kimmo Löfman <ext.kimmo.lofman@varma.fi>
2024-12-07 15:03:09 +0200

    Added 2.01 exercise

---
+++ part-2/exercise-2.01/log-output-app/index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+
+const stringGenerator = (length)=> {
+    const chars = 'abcdefghijklmnopqrstuvwxyz';
+    let result = '';
+    const charLength = chars.length;
+
+    for (let i = 0; i < length; i++) {
+        result += chars.charAt(Math.floor(Math.random() * charLength));
+    }
+
+    return result;
+}
+const randomString = stringGenerator(20);
+
+app.get('/', async (req, res) => {
+    try {
+      
+      const pingpongUrl = 'http://localhost:3001/pingpong'; 
+  
+      const response = await axios.get(pingpongUrl);
+  
+      if (response.status === 200 && response.data.counter !== undefined) {
+        const counterValue = response.data.counter;
+        const timestamp = new Date().toISOString(); 
+
+        const htmlResponse = `
+          <!DOCTYPE html>
+          <html lang="en">
+          <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>logout pingpong demo</title>
+          </head>
+          <body>
+            <h1>Current Counter Value</h1>
+            <p>${timestamp}: ${randomString}</p>
+            <p>Ping / Pongs: ${counterValue}</p>
+          </body>
+          </html>
+        `;
+  
+        // Send the HTML response to the client
+        res.send(htmlResponse);
+      } else {
+        res.status(500).send('Failed to retrieve counter value.');
+      }
+    } catch (error) {
+      console.error('Error fetching counter value:', error);
+      res.status(500).send('An error occurred while fetching the counter value.');
+    }
+  });
+
+
+
+app.listen(port, () => {
+    console.log(`Server started in port ${port}`);
+  });
